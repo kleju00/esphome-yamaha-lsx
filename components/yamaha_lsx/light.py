6f07f538cffb9a5f13abbc5ad914a1bf0759bd7e
kleju00 <50960830+kleju00@users.noreply.github.com>
2026-09-20 18:53:09 +0200

    Update light.py

--- components/yamaha_lsx/light.py
+++ components/yamaha_lsx/light.py
@@ -3,7 +3,6 @@ import esphome.config_validation as cv
 from esphome.components import light, esp32
 from esphome.const import CONF_OUTPUT_ID
 
-# Deklarujemy nową stałą dla pola MAC
 CONF_MAC_ADDRESS = "mac_address"
 
 yamaha_lsx_ns = cg.esphome_ns.namespace('yamaha_lsx')
@@ -11,12 +10,10 @@ YamahaLSX = yamaha_lsx_ns.class_('YamahaLSX', cg.Component, light.LightOutput)
 
 CONFIG_SCHEMA = light.BRIGHTNESS_ONLY_LIGHT_SCHEMA.extend({
     cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(YamahaLSX),
-    cv.Required(CONF_MAC_ADDRESS): cv.mac_address, # Wymagamy podania adresu MAC
+    cv.Required(CONF_MAC_ADDRESS): cv.mac_address,
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
-    # Wymuszenie dodania modułu 'bt' do budowy ESP-IDF (Bluetooth Classic/SPP)
-    esp32.add_idf_component("bt")
     esp32.include_builtin_idf_component("bt")
 
     var = cg.new_Pvariable(config[CONF_OUTPUT_ID])
@@ -24,6 +21,5 @@ async def to_code(config):
     await cg.register_component(var, config)
     await light.register_light(var, config)
 
-    # Przekazanie adresu MAC z YAMLa do C++
     mac = config[CONF_MAC_ADDRESS].parts
     cg.add(var.set_mac_address([mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]]))
